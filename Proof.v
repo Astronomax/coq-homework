ae103c0116dfd4c65a16a962161c9ef4496a8e87
Astronomax <fgfgfb93@gmail.com>
2022-04-01 22:35:58 +0300

    Complete homework

--- Proof.v
+++ Proof.v
@@ -12,7 +12,11 @@ Section Logic.
 
 Definition and_via_ex (A B : Prop) :
   (exists (_ : A), B) <-> A /\ B
-:= provide_solution.
+:= conj 
+    (fun EXx => match EXx with
+      ex_intro _ x Px => conj x Px 
+    end)
+    (fun aCb => ex_intro _ ((proj1 aCb) : A) ((proj2 aCb) : B)).
 
 (** Exercise: The dual Frobenius rule *)
 
@@ -38,33 +42,50 @@ Notation "f =1 g" := (forall x, f x = g x) (at level 70, no associativity).
 (** Exercise : associativity of function composition *)
 Definition compA (f : A -> B) (g : B -> C) (h : C -> D) :
   (h \o g) \o f = h \o (g \o f)
-:= provide_solution.
+:= eq_refl (fun x => h (g (f x))).
 
 (** Exercise: Reflexivity *)
 Definition eqext_refl :
-  forall (f : A -> B), f =1 f
-:= provide_solution.
+forall (f : A -> B), f =1 f
+:= fun x y => eq_refl (x y).
 
 (** Exercise: Symmetry *)
 Definition eqext_sym :
-  forall (f g : A -> B), f =1 g -> g =1 f
-:= provide_solution.
+forall (f g : A -> B), f =1 g -> g =1 f
+:= fun f g fEQg x =>
+match
+(fEQg x)
+with
+| eq_refl => eq_refl (f x)
+end.
 
 (** Exercise: Transitivity *)
 Definition eqext_trans :
   forall (f g h : A -> B), f =1 g -> g =1 h -> f =1 h
-:= provide_solution.
+:= fun f g h fEQg gEQh x => 
+      match (eqext_sym f g fEQg) x
+      with 
+      | eq_refl => match gEQh x with 
+        | eq_refl => eq_refl (g x)
+      end
+        end.
 
 (** Exercise: left congruence *)
 Definition eq_compl :
   forall (f g : A -> B) (h : B -> C),
     f =1 g -> h \o f =1 h \o g
-:= provide_solution.
+:= fun f g h fEQg x => match fEQg x 
+      with 
+      | eq_refl => eq_refl (h (f x)) 
+      end.
 
 (** Exercise: right congruence *)
 Definition eq_compr :
   forall (f g : B -> C) (h : A -> B),
     f =1 g -> f \o h =1 g \o h
-:= provide_solution.
+:= fun f g h fEQg x => match fEQg (h x) 
+      with 
+      | eq_refl => eq_refl (f (h x)) 
+      end.
 
 End ExtensionalEqualityAndComposition.
